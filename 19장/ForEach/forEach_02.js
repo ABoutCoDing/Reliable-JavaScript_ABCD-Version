2712c50ec8bdaed7b25d4ec01fafd03bcdfd22d5
jamie han <iheart79@gmail.com>
2018-02-24 22:25:08 +0900

    c

--- 19장/ForEach/forEach_02.js
+++ 19장/ForEach/forEach_02.js
@@ -9,10 +9,10 @@ Conference.polyfills.arrayForEach = function(callbackFcn, thisObj) {
   }
 };
 
-Aop.before('arrayForEach',function isObjectWithLength(obj) {
+Aop.before('arrayForEach', function isObjectWithLength(obj) {
   if (typeof(this) !== 'object' ||
   !(typeof this.length === 'number' && isFinite(this.length) &&
   Math.floor(this.length) === this.length && this.length >= 0)) {
     throw new Error('arrayForEach의 호출부는 유사 배열 타입이어야 합니다.');
   }
-},Conference.polyfills);
+}, Conference.polyfills);
